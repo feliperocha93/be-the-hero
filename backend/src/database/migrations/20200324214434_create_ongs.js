23a021e2b20a5d56932a49f75d80aa7967ad9485
Felipe <fee.rocha@hotmail.com>
2020-05-06 22:02:14 -0300

    Adicionando campo password no db

--- backend/src/database/migrations/20200324214434_create_ongs.js
+++ backend/src/database/migrations/20200324214434_create_ongs.js
@@ -1,14 +1,15 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable("ongs", table => {
-    table.string("id").primary();
+    table.increments("id");
     table.string("name").notNullable();
     table.string("email").notNullable();
     table.string("whatsapp").notNullable();
     table.string("city").notNullable();
     table.string("uf", 2).notNullable();
+    table.string("password").notNullable();
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable("ongs");
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,6 +15,7 @@ routes.post("/ongs", celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
+    password: Joi.string().required().min(6).max(8),
     whatsapp: Joi.string().required().min(10).max(11),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2)
