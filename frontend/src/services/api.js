5638d220ad44b7a018623796640bdce5906c0f1a
Felipe <fee.rocha@hotmail.com>
2020-05-08 00:52:26 -0300

    Correção no endereço da api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://bth-node-api.herokuapp.com/',
-  baseURL: 'http://localhost:3333/',
+  baseURL: 'https://bth-node-api.herokuapp.com/',
 });
 
 export default api;
